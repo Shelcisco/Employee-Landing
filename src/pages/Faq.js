78fa35e6731992f1ca7053e3a7f6318ccb420364
Shelcisco <119394650+Shelcisco@users.noreply.github.com>
2023-11-06 16:55:04 -0800

    ADD FAQ page

---
+++ src/pages/Faq.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+export default function Faq() {
+  return (
+    <div className="faq-page">
+      <h1>Frequently Asked Questions</h1>
+      <div className="faq-question">
+        <h2>Question 1</h2>
+        <p>Answer to Question 1</p>
+        <p>For additional help, reach out to <a href="mailto:shel.cisco5@gmail.com">shel.cisco5@gmail.com</a></p>
+      </div>
+      <div className="faq-question">
+        <h2>Question 2</h2>
+        <p>Answer to Question 2</p>
+      </div>
+      <div className="faq-question">
+        <h2>Question 2</h2>
+        <p>Answer to Question 2</p>
+      </div>
+      <div className="faq-question">
+        <h2>Question 2</h2>
+        <p>Answer to Question 2</p>
+      </div>
+      <div className="faq-question">
+        <h2>Question 2</h2>
+        <p>Answer to Question 2</p>
+      </div>
+      <div className="faq-question">
+        <h2>Question 2</h2>
+        <p>Answer to Question 2</p>
+      </div>
+      <div className="faq-question">
+        <h2>Question 2</h2>
+        <p>Answer to Question 2</p>
+      </div>
+      <div className="faq-question">
+        <h2>Question 2</h2>
+        <p>Answer to Question 2</p>
+      </div>
+      <div className="faq-question">
+        <h2>Question 2</h2>
+        <p>Answer to Question 2</p>
+      </div>
+      <div className="faq-question">
+        <h2>BLAH</h2>
+        <p>Answer to Question 2</p>
+      </div>
+      {/* You can add more questions and answers here */}
+    </div>
+  );
+}
+
