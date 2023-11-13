53562fd95237340a0ba6b8af40ecf5072f3ee68f
Shelcisco <119394650+Shelcisco@users.noreply.github.com>
2023-11-12 22:26:45 -0800

    ADD Email/Phone links on Faq.js

--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -3,49 +3,30 @@ import React from "react";
 export default function Faq() {
   return (
     <div className="faq-page">
+        <div id="faq-head">
       <h1>Frequently Asked Questions</h1>
+      </div>
       <div className="faq-question">
         <h2>Question 1</h2>
         <p>Answer to Question 1</p>
-        <p>For additional help, reach out to <a href="mailto:shel.cisco5@gmail.com">shel.cisco5@gmail.com</a></p>
+        <p>For additional help, reach out to Customer Service and District Manager blank</p>
+           <p><a href="mailto:shel.cisco5@gmail.com;shelbyfrancisco@hotmail.com">Email</a>, Phone <a href="tel:206-554-9213">206-554-9213</a></p> 
       </div>
+      <hr></hr>
       <div className="faq-question">
         <h2>Question 2</h2>
-        <p>Answer to Question 2</p>
+        <p>Answer to Question </p>
       </div>
+      <hr></hr>
       <div className="faq-question">
-        <h2>Question 2</h2>
-        <p>Answer to Question 2</p>
+        <h2>Question 3</h2>
+        <p>Answer to Question </p>
       </div>
+      <hr></hr>
       <div className="faq-question">
-        <h2>Question 2</h2>
-        <p>Answer to Question 2</p>
+        <h2>Question 4</h2>
+        <p>Answer to Question </p>
       </div>
-      <div className="faq-question">
-        <h2>Question 2</h2>
-        <p>Answer to Question 2</p>
-      </div>
-      <div className="faq-question">
-        <h2>Question 2</h2>
-        <p>Answer to Question 2</p>
-      </div>
-      <div className="faq-question">
-        <h2>Question 2</h2>
-        <p>Answer to Question 2</p>
-      </div>
-      <div className="faq-question">
-        <h2>Question 2</h2>
-        <p>Answer to Question 2</p>
-      </div>
-      <div className="faq-question">
-        <h2>Question 2</h2>
-        <p>Answer to Question 2</p>
-      </div>
-      <div className="faq-question">
-        <h2>BLAH</h2>
-        <p>Answer to Question 2</p>
-      </div>
-      {/* You can add more questions and answers here */}
     </div>
   );
 }
