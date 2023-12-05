6ee3d1e145b99d46da64d0ef39e3c5ea2dcd3a2e
Shelcisco <119394650+Shelcisco@users.noreply.github.com>
2023-12-04 22:31:35 -0800

    Link Fav.js to WA.js

--- src/pages/WA.js
+++ src/pages/WA.js
@@ -2,40 +2,42 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Faq from "./Faq"; // Import the Faq component
 import Bs from "./Bs"; // Import the Best Sales component
+import Fav from "./Favorite"; 
 
 export default function Navbar({ page, setPage }) {
   // State to keep track of the currently open sidebar option
   const [activeOption, setActiveOption] = useState(null);
   const [showFaq, setShowFaq] = useState(false);
   const [showBs, setShowBs] = useState(false);
+  const [showFav, setShowFav] = useState(false);
 
   // Function to toggle the sidebar menu for a specific option
   const toggleSidebar = (option) => {
     if (activeOption === option) {
       // If the same option is clicked again, close the sidebar
       setActiveOption(null);
-      // Also, hide the FAQ and Bs pages
       setShowFaq(false);
       setShowBs(false);
+      setShowFav(false);
     } else {
-      // Otherwise, open the new option and close the previous one
       setActiveOption(option);
 
-      // Check if the option is "Frequently Asked"
       if (option === "faq") {
-        // Show the FAQ page
         setShowFaq(true);
-        // Hide the Bs page for other options
         setShowBs(false);
+        setShowFav(false);
       } else if (option === "Bs") {
-        // Show the Bs page
         setShowBs(true);
-        // Hide the FAQ page for other options
         setShowFaq(false);
-      } else {
-        // Hide the FAQ and Bs pages for other options
+        setShowFav(false);
+      } else if (option === "Fav") {
+        setShowFav(true);
         setShowFaq(false);
         setShowBs(false);
+      } else {
+        setShowFaq(false);
+        setShowBs(false);
+        setShowFav(false);
       }
     }
   };
@@ -74,7 +76,16 @@ export default function Navbar({ page, setPage }) {
         >
           Sales
         </a>
+ <a 
+          href="#fav"
+          onClick={() => toggleSidebar("Fav")}
+          className={activeOption === "Fav" ? "active" : ""}
+        >
+          Favorites
+        </a>
       </div>
+
+
       <div className="sidebar">
         {activeOption === "sales" && (
           <div className="sidenav">
@@ -113,6 +124,7 @@ export default function Navbar({ page, setPage }) {
       <div className="main">
         {showFaq ? <Faq /> : null} {/* Conditionally render the FAQ component */}
         {showBs ? <Bs /> : null} {/* Conditionally render the Bs component */}
+        {showFav ? <Fav /> : null} {/* Conditionally render the Fav component */}
       </div>
     </div>
   );
