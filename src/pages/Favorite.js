2e05965d68d32035db55f64b9727c6a50201bf9e
Shelcisco <119394650+Shelcisco@users.noreply.github.com>
2024-01-04 00:09:15 -0800

    Update lpage links, import FavoriteContext

--- src/pages/Favorite.js
+++ src/pages/Favorite.js
@@ -1,32 +1,38 @@
-
 import React from "react";
 import BsPic1 from '../assets/BsPic1.png';
 import FAQpic from '../assets/FAQpic.png';
 import { Link } from "react-router-dom";
+import Navbar from "./Nav";
+import { useFavorites } from "./FavoritesContext"; // Import the context
 
-export default function Fav({ handleLinkClick }) {
+export default function Fav() {
+  const { isFavorite } = useFavorites();
+
   return (
     <div className="fav">
-      <div className="plac">
-      </div>
-     
-      <div className="card">
-        <Link to="/WA#Bs" onClick={() => handleLinkClick("Bs")}>
-          <img src={BsPic1} width={250} height={200} alt="BsFav" />
-        </Link>
-        <div className="container">
-        <Link to="/WA#Bs" onClick={() => handleLinkClick("Bs")}><h4>Succesful Salesperson SOP</h4></Link>
-        </div>
-      </div>
+      <Navbar />
 
-      <div className="card">
-        <Link to="/WA#Faq" onClick={() => handleLinkClick("Faq")}>
-          <img src={FAQpic} width={250} height={200} alt="BsFav" />
-        </Link>
-        <div className="container">
-        <Link to="/WA#Faq" onClick={() => handleLinkClick("Faq")}><h4>Frequently Asked Questions</h4></Link>
+      {isFavorite("Bs") && (
+        <div className="card" alt="bsCard">
+          <Link to="/Bs">
+            <img src={BsPic1} width={250} height={200} alt="BsFav" />
+          </Link>
+          <div className="container">
+            <Link to="/Bs"><h4>Succesful Salesperson SOP</h4></Link>
+          </div>
         </div>
-      </div>
+      )}
+
+      {isFavorite("Faq") && (
+        <div className="card" alt="faqCard">
+          <Link to="/Faq">
+            <img src={FAQpic} width={250} height={200} alt="FaqFav" />
+          </Link>
+          <div className="container">
+            <Link to="/Faq"><h4>Frequently Asked Questions</h4></Link>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
